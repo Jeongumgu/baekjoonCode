3ae7779a43ee666de8f1ee5b100f34b8f92cb599
Jeongumgu <33191329+Jeongumgu@users.noreply.github.com>
2023-09-05 15:55:51 +0900

    Create 11718.java

---
+++ 11718.java
@@ -0,0 +1,17 @@
+/**
+ * 11718번 그대로 출력하기
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = "";
+        while((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
