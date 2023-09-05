9b3e534de2377ce9de19dcd94c6fef064071c258
Jeongumgu <33191329+Jeongumgu@users.noreply.github.com>
2023-09-05 15:52:14 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public void main (String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
